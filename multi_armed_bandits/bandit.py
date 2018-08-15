fixed variance

multi_armed_bandits/bandit.py
@@ -1,5 +1,4 @@
 import math
-from random import gauss, uniform
 import numpy as np
 
 
@@ -10,8 +9,8 @@ class bandit:
     self.variance = variance
 
     self.step = 0
-    self.qTable = np.full(kArm, initial)
-    self.armMeans = [uniform(min, max) for i in range(self.kArm)]
+    self.qTable = np.full(self.kArm, initial)
+    self.armMeans = np.random.uniform(min, max, self.kArm)
 
   def takeAction(self):
     if self.epsilon > 0 and np.random.binomial(1, self.epsilon) == 1:
@@ -19,7 +18,8 @@ class bandit:
     else:
       idx = np.argmax(self.qTable)
 
-    reward = gauss(self.armMeans[idx], math.sqrt(self.variance))
+    reward = np.random.normal(self.armMeans[idx], self.variance)
+
     self.step += 1
     self.qTable[idx] +=  (1 / self.step) * (reward - self.qTable[idx])
 

multi_armed_bandits/policy.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 run = 2000
 ts = 1000
 
-epsilon = 0.1
-variance = 1
-limMin = -1
+variance = 0.5
+limMin = 0
 limMax = 5
 
 def greedyPolicy():
@@ -22,7 +21,7 @@ def greedyPolicy():
 
 	return averageRewards / run
 
-def epsilonGreedyPolicy():
+def epsilonGreedyPolicy(epsilon):
 	averageRewards = np.zeros(ts)
 
 	for num in range(run):
@@ -34,6 +33,7 @@ def epsilonGreedyPolicy():
 	return averageRewards / run
 
 plt.plot(greedyPolicy(), color='r')
-plt.plot(epsilonGreedyPolicy(), color='b')
+plt.plot(epsilonGreedyPolicy(0.1), color='b')
+plt.plot(epsilonGreedyPolicy(0.01), color='g')
 
 plt.show()